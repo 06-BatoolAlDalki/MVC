0b0018e27b7716dda5b7b79d2a59102d65a88b20
Batool <batool.com>
2023-02-01 16:12:42 +0300

    task1_2

---
+++ 1_2_Morining_MVC/1_2_Morining_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1_2_Morining_MVC.MvcApplication" Language="C#" %>
---
+++ Task_1_2_mvc/Task_1_2_mvc/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task_1_2_mvc.Models;
+
+namespace Task_1_2_mvc.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+
+            List<Employee> emp = new List<Employee>();
+            emp.Add(new Employee { ID = 1, Name = "Batool", Major = "Network", Faculty = "JUST" });
+            emp.Add(new Employee { ID = 2, Name = "Rama", Major = "Finace", Faculty = "Yurmouk" });
+
+           
+            return View(emp);
+        }
+    }
+}

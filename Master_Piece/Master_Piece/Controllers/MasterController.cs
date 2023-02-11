77cf414b6f8734972e1e6a3bbe32ca79e0770cc8
Batool <batool.com>
2023-02-11 14:39:22 +0300

    master

---
+++ Master_Piece/Master_Piece/Controllers/MasterController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Master_Piece.Controllers
+{
+    public class MasterController : Controller
+    {
+        // GET: Master
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+
+        public ActionResult Category()
+        {
+            return View();
+        }
+
+        public ActionResult properity()
+        {
+            return View();
+        }
+
+        public ActionResult Singleproperity()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        public ActionResult PayBuyer()
+        {
+            return View();
+        }
+
+        public ActionResult Subscribe()
+        {
+            return View();
+        }
+
+        public ActionResult ProfileUSer()
+        {
+            return View();
+        }
+
+        public ActionResult AddHouse()
+        {
+            return View();
+        }
+
+        public ActionResult aboutus()
+        {
+            return View();
+        }
+
+        public ActionResult contactus()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult DashBoard()
+        {
+            return View();
+        }
+    }
+}
---
+++ Master_Piece/Master_Piece/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Master_Piece.Startup))]
+namespace Master_Piece
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
